8c0a0c30bd3ce636b11933a2773fa18e1e03a446
brody <brody@steidley.org>
2026-07-21 13:12:56 -0600

    remove TODO comments

--- src/main/kotlin/ui/ControlPanel.kt
+++ src/main/kotlin/ui/ControlPanel.kt
@@ -65,10 +65,6 @@ class ControlPanel(
     }
 
     private fun drive(left: Double, right: Double) {
-        // TODO(student): build one of YOUR Command classes for this action and run it via the API:
-        //     api.perform(MySetVelocityCommand(api.actuator, left, right))
-        // `left` / `right` are the intended track velocities for the button that was pressed
-        // (e.g. Forward = (speed, speed), Left = (turn, -turn)). Design whatever command set you like.
 	api.perform(SetVelocityCommand(api.actuator, left, right))
     }
 
